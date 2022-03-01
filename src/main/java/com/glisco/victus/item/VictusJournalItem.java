f5bcafca21fc7b258c7e449c7f8fb364eca6ff02
glisco <glisco03@web.de>
2022-03-01 19:24:00 +0100

    initial port to 1.18.2

--- src/main/java/com/glisco/victus/item/VictusJournalItem.java
+++ src/main/java/com/glisco/victus/item/VictusJournalItem.java
@@ -4,15 +4,10 @@ import com.glisco.victus.Victus;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
-import vazkii.patchouli.api.PatchouliAPI;
-import vazkii.patchouli.common.base.PatchouliSounds;
-import vazkii.patchouli.common.book.Book;
-import vazkii.patchouli.common.book.BookRegistry;
 
 public class VictusJournalItem extends Item {
 
@@ -25,12 +20,15 @@ public class VictusJournalItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getStackInHand(hand);
-        Book book = BookRegistry.INSTANCE.books.get(VICTUS_GUIDE);
 
-        if (!world.isClient()) {
-            PatchouliAPI.get().openBookGUI((ServerPlayerEntity) player, book.id);
-            player.playSound(PatchouliSounds.getSound(book.openSound, PatchouliSounds.BOOK_OPEN), 1, (float) (0.7 + Math.random() * 0.4));
-        }
+        // TODO re-add patchouli support
+
+//        Book book = BookRegistry.INSTANCE.books.get(VICTUS_GUIDE);
+//
+//        if (!world.isClient()) {
+//            PatchouliAPI.get().openBookGUI((ServerPlayerEntity) player, book.id);
+//            player.playSound(PatchouliSounds.getSound(book.openSound, PatchouliSounds.BOOK_OPEN), 1, (float) (0.7 + Math.random() * 0.4));
+//        }
 
         return TypedActionResult.success(stack);
     }
